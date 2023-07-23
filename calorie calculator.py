cc2b21bff2ea0d857df20fcd6de15fe9a7217d6d
Richard Solomon <35183675+RichieSolo@users.noreply.github.com>
2023-07-23 18:59:28 -0400

    Got rid of unused file.  rewrote main and fixed functions

--- calorie calculator.py
+++ calorie calculator.py
@@ -1,25 +1,12 @@
 
 """
 = = = = = = = = 
-Monster.C 
-Taking and holding user data
+Richie.S 
+Start 06/40/2019 
+
+
 = = = = = = = = 
 """
-print ('CalorieCalculator')
-
-''' TAKE USER DATA'''
-
-G=["M","F"]
-GENDER=input("Are you male[M] or female[F]?")
-AGE=float(input('What"s your age?'))
-WEIGHT =float(input('How much do you weigh?'))
-HEIGHT=float(input('How tall are you?'))
-WEIGHT=WEIGHT/2.2
-HEIGHT=HEIGHT*2.554
-L=['S','B','C']
-LEVEL=input("Are you Maintaining[S],bulking[B], or cutting[C]")
-FemaleCalorieCalculator= 65.1+ (9.563 * WEIGHT) + (1.850 * HEIGHT) - (4.67*AGE)
-MaleCalorieRecomendation=66.5 + (13.75 * WEIGHT) + (5.003 *HEIGHT) - (6.775 *AGE)
 
 def maintainM():
      print(MaleCalorieRecomendation)
@@ -41,27 +28,48 @@ def cutF():
      print(FemaleCalorieCalculator-200)
 
 
-for T in L:
-     if LEVEL==("S")
-          maintainM():
 
-for T in L:
-     if LEVEL==("B")
-          gainM():
 
-for T in L:
-     IF LEVEL:==('C')
-          cutM():
+def main():
+
+     
+     print ('CalorieCalculator')
+     G=["M","F"]
+     GENDER=input("Are you male[M] or female[F]?")
+     AGE=float(input('What"s your age?'))
+     WEIGHT =float(input('How much do you weigh?'))
+     HEIGHT=float(input('How tall are you?'))
+
+
+     WEIGHT=WEIGHT/2.2
+     HEIGHT=HEIGHT*2.554
+     L=['S','B','C']
+     LEVEL=input("Are you Maintaining[S],bulking[B], or cutting[C]")
+     FemaleCalorieCalculator= 65.1+ (9.563 * WEIGHT) + (1.850 * HEIGHT) - (4.67*AGE)
+     MaleCalorieRecomendation=66.5 + (13.75 * WEIGHT) + (5.003 *HEIGHT) - (6.775 *AGE)
+     for T in L:
+          if LEVEL==("S"):
+               maintainM()
+
+     for T in L:
+          if LEVEL==("B"):
+               gainM()
+
+     for T in L:
+          if LEVEL==('C'):
+               cutM()
+                    
+
                
 
-          
+     for Q in G:
+          if (GENDER == ("M")):
+               continue
+               
+               
+          else: FemaleCalorieCalculator
 
-for Q in G:
-     if  GENDER == ("M"):
-          
-          
-     else : FemaleCalorieCalculator
-
+main()
 
 
 
